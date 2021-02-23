2128389ecf23b6929e1b83c8b5fa56f92ab9d072
fukiran <macdorphoto@yahoo.com>
2021-02-23 10:17:37 +0000

    add employee

--- src/main/java/com/example/demo/models/Employee.java
+++ src/main/java/com/example/demo/models/Employee.java
@@ -1,4 +1,63 @@
 package com.example.demo.models;
 
+import java.util.ArrayList;
+
 public class Employee {
+
+    private String first_name;
+    private String last_name;
+    private int employeeNr;
+    private Long id;
+    private ArrayList<Project>projects;
+
+    public Employee(String first_name, String last_name, int employeeNr) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.employeeNr = employeeNr;
+        this.projects = new ArrayList<>();
+    }
+
+    public void addProject(Project project){
+        this.projects.add(project);
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getEmployeeNr() {
+        return employeeNr;
+    }
+
+    public void setEmployeeNr(int employeeNr) {
+        this.employeeNr = employeeNr;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ArrayList<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(ArrayList<Project> projects) {
+        this.projects = projects;
+    }
 }
