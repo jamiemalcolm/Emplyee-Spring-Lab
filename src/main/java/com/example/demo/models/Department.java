aec7ebeb13bb7d99bff40e922432e6215be22cff
fukiran <macdorphoto@yahoo.com>
2021-02-23 10:22:50 +0000

    add department

--- src/main/java/com/example/demo/models/Department.java
+++ src/main/java/com/example/demo/models/Department.java
@@ -1,6 +1,43 @@
 package com.example.demo.models;
 
+import java.util.ArrayList;
+
 public class Department {
 
+private String name;
+private ArrayList<Employee>employees;
+private Long id;
 
+    public Department(String name) {
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+
+    public void addEmployee(Employee employee){
+        this.employees.add(employee);
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
