a533ba151ba7c7075d023cc641a1a28142475c43
fukiran <macdorphoto@yahoo.com>
2021-02-23 10:44:05 +0000

    add one to many

--- src/main/java/com/example/demo/models/Department.java
+++ src/main/java/com/example/demo/models/Department.java
@@ -1,18 +1,33 @@
 package com.example.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 
+@Entity
+@Table(name = "departments")
 public class Department {
 
+
+@Column(name = "name")
 private String name;
+
+@JsonIgnoreProperties({"department"})
+@OneToMany(mappedBy = "department")
 private ArrayList<Employee>employees;
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
+
 
     public Department(String name) {
         this.name = name;
         this.employees = new ArrayList<>();
     }
 
+    public Department(){}
 
     public void addEmployee(Employee employee){
         this.employees.add(employee);
--- src/main/java/com/example/demo/models/Employee.java
+++ src/main/java/com/example/demo/models/Employee.java
@@ -1,24 +1,54 @@
 package com.example.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 
+@Entity
+@Table(name="employees")
 public class Employee {
 
+    @Column(name="first_name")
     private String first_name;
+
+    @Column(name="last_name")
     private String last_name;
+
+    @Column(name="employeeNr")
     private int employeeNr;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private ArrayList<Project>projects;
 
-    public Employee(String first_name, String last_name, int employeeNr) {
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    @JsonIgnoreProperties({"employees"})
+    private Department department;
+
+
+    public Employee(String first_name, String last_name, int employeeNr, Department department) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.employeeNr = employeeNr;
         this.projects = new ArrayList<>();
+        this.department = department;
     }
+
+    public Employee(){}
 
     public void addProject(Project project){
         this.projects.add(project);
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 
     public String getFirst_name() {
