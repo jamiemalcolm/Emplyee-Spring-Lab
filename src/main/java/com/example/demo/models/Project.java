e6a672ca799b262123e059933a4ddabac3ac832f
fukiran <macdorphoto@yahoo.com>
2021-02-23 10:49:44 +0000

    add project

--- src/main/java/com/example/demo/models/Project.java
+++ src/main/java/com/example/demo/models/Project.java
@@ -1,4 +1,43 @@
 package com.example.demo.models;
 
+import java.util.ArrayList;
+
 public class Project {
+
+    private String name;
+    private ArrayList<Employee>employees;
+    private Long id;
+
+    public Project(String name) {
+        this.name = name;
+        this.employees = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee){
+        this.employees.add(employee);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
