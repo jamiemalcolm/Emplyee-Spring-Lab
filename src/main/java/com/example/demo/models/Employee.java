5423196f116adb11158d209cb2ea67f3e18dc1e9
fukiran <macdorphoto@yahoo.com>
2021-02-23 11:02:54 +0000

    add many to many

--- src/main/java/com/example/demo/models/Employee.java
+++ src/main/java/com/example/demo/models/Employee.java
@@ -1,6 +1,7 @@
 package com.example.demo.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -21,12 +22,29 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToMany
+    @JsonIgnoreProperties({"employee"})
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+            name ="employees_projects",
+            joinColumns = { @JoinColumn(
+                    name = "employee_id",
+                    nullable = false,
+            )},
+            inverseJoinColumns = { @JoinColumn(
+                    name = "project_id",
+                    nullable = false,
+                    updatable = false
+            )}
+    )
     private ArrayList<Project>projects;
 
     @ManyToOne
     @JoinColumn(name = "department_id", nullable = false)
     @JsonIgnoreProperties({"employees"})
     private Department department;
+
 
 
     public Employee(String first_name, String last_name, int employeeNr, Department department) {
--- src/main/java/com/example/demo/models/Project.java
+++ src/main/java/com/example/demo/models/Project.java
@@ -1,17 +1,45 @@
 package com.example.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 
+@Entity
+@Table(name="projects")
 public class Project {
 
+    @Column(name="name")
     private String name;
+
+    @ManyToMany
+    @JsonIgnoreProperties({"project"})
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+            name ="projects_employees",
+            joinColumns = { @JoinColumn(
+                    name = "project_id",
+                    nullable = false,
+            )},
+            inverseJoinColumns = { @JoinColumn(
+                    name = "employee_id",
+                    nullable = false,
+                    updatable = false
+            )}
+    )
     private ArrayList<Employee>employees;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Project(String name) {
         this.name = name;
         this.employees = new ArrayList<>();
     }
+
+    public Project(){}
 
     public void addEmployee(Employee employee){
         this.employees.add(employee);
